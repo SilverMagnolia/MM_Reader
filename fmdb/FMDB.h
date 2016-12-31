6fe2c5524e06c97c3ad40352bc7c651c7f6f88fc
박종호 <bagjongho@bagjonghoui-macbook-pro.local>
2016-12-31 15:24:54 +0900

    additional commit for removing warning message

---
+++ fmdb/FMDB.h
@@ -0,0 +1,22 @@
+//
+//  FMDB.h
+//  FMDB
+//
+//  Created by 박종호 on 2016. 12. 29..
+//  Copyright © 2016년 박종호. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FMDB.
+FOUNDATION_EXPORT double FMDBVersionNumber;
+
+//! Project version string for FMDB.
+FOUNDATION_EXPORT const unsigned char FMDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMDB/PublicHeader.h>
+
+#import "FMDatabase.h"
+#import "FMResultSet.h"
+#import "FMDatabaseAdditions.h"
+#import "FMDatabaseQueue.h"
